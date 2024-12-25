e310e39ade168bef35c5eeab869830fc4e05e0d5
YlAN9854 <11062543+ylan9854@user.noreply.gitee.com>
2024-12-25 13:34:00 +0800

    Zelin：第二次作业提交

---
+++ 22451238李泽霖/second_work/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import SecondWork from './components/SecondWork.vue'
+</script>
+
+<template>
+  <div>
+    <SecondWork />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
